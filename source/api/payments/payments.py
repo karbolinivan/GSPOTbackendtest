bee9f10d1c22eecd8a3b4ab2760e7d848ba30d7e
Kamila Adigamova <kamila.adigamov@gmail.com>
2023-07-15 23:49:39 +0200

    fixes wip

--- source/api/payments/payments.py
+++ source/api/payments/payments.py
@@ -6,17 +6,17 @@ from source.enums.payments.payments import External_Payments
 
 
 @allure.step('Get services list')
-def get_services_list(auth=tester_auth):
+def get_services_list(auth=None):
     response = Requests.get(url=External_Payments.SERVICES, auth=auth)
     return response
 
 @allure.step('Create service')
-def create_service(json, auth=tester_auth, name=""):
+def create_service(json, auth=None, name=""):
     response = Requests.post(url=External_Payments.SERVICES, json=json, auth=auth)
     return response
 
 @allure.step('Delete service')
-def delete_service(service_id, auth=tester_auth):
+def delete_service(service_id, auth=None):
     url = f"{External_Payments.SERVICES}/{service_id}"
     response = Requests.delete(url=url, auth=auth)
     return response
--- tests/payments/external_payments/services/test_payments_services_post_positive.py
+++ tests/payments/external_payments/services/test_payments_services_post_positive.py
@@ -14,33 +14,6 @@ class TestPaymentsPostList:
     @allure.title('Tests of the creating new external payments services')
     @allure.description('Проверка успешного ответа [201] при создании сервиса оплаты с именем {0}')
     @pytest.mark.parametrize("name, expected, message", [
-        ("مثال للزخرفة", 201, None),
-        ("WeBmAnY", 201, None),
-        ("вэб мани", 201, None),
-        ("qiwi", 201, None),
-        ("fhjthgld3845unfjlcns30567fgcjk", 201, None),
-        ("56565", 201, None),
-        ("&*^%$", 201, None),
-        ("four ", 201, None),
-        (" two", 201, None),
-        ("bank card", 201, None),
-    ])
-    def test_external_payments_services_positive_input_value_post(self, name, expected, message):
-        response = create_service({"name": name})
-
-        assert_status_code(response=response, expected=expected)
-        assert_json_by_model(response=response, model=External_Payments)
-
-        service_id = response.json().get("id")
-        if service_id:
-            delete_service(service_id)
-
-        print(response.text)
-
-
-
-    @allure.title('Tests for deleting external payments services')
-    @pytest.mark.parametrize("name", [
         "مثال للزخرفة",
         "WeBmAnY",
         "вэб мани",
@@ -51,23 +24,18 @@ class TestPaymentsPostList:
         "four ",
         " two",
         "bank card",
+
     ])
-    def test_external_payments_services_delete(self, name):
-        # Get the services list
-        response = get_services_list()
-        assert response.status_code == 200, "Failed to retrieve services list"
+    def test_external_payments_services_positive_input_value_post(self, name, expected, message):
+        response = create_service({"name": name})
 
-        # Find the service ID based on the service name
-        services = response.json()
-        service_id = None
-        for service in services:
-            if service["name"] == name:
-                service_id = service["id"]
-                break
+        assert_status_code(response=response, expected=expected)
+        assert_json_by_model(response=response, model=External_Payments)
+        print(response.json())
 
-        # Delete the service using the service ID
-        assert service_id is not None, f"Failed to find service with name: {name}"
-        delete_response = delete_service(service_id)
-        assert delete_response.status_code == 200, f"Failed to delete service with name: {name}"
+        service_id = response.json().get("id")
+        if service_id:
+            delete_response = delete_service(service_id)
+            assert delete_response.status_code == 200, "Unable to delete"
+
         print(response.text)
-        print(123)
