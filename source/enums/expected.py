fb76f1622d78ccd24132a3ae7ee7a57e5a53d06a
VasyaQAr <spartak69nel@gmail.com>
2023-08-09 22:00:51 +0300

    Payments | add tests owner

--- source/enums/expected.py
+++ source/enums/expected.py
@@ -15,6 +15,16 @@ class ExpectedJSON(Enum):
             "detail": "Not found."
         }
 
+    PAYMENT_ACCOUNTS_OWNER_LESS_THAN_ALLOWED_COMMISSION = ["Ensure this value is greater than or equal to 0."]
+    PAYMENT_ACCOUNTS_OWNER_EXCEEDED_LENGTH_COMMISSION = ["Ensure this value is less than or equal to 100."]
+    PAYMENT_ACCOUNTS_OWNER_INVALID_VALUE_COMMISSION = ["A valid number is required."]
+    PAYMENT_ACCOUNTS_OWNER_MORE_THAN_2_DECIMAL_PLACES_COMMISSION = ["Ensure that there are no more than 2 decimal places."]
+    PAYMENT_ACCOUNTS_OWNER_MORE_THAN_5_DIGITS_IN_TOTAL_COMMISSION = ["Ensure that there are no more than 5 digits in total."]
+    PAYMENT_ACCOUNTS_OWNER_INVALID_VALUE_FROZEN_TIME = ["Duration has wrong format. Use one of these formats instead: [DD] [HH:[MM:]]ss[.uuuuuu]."]
+    PAYMENT_ACCOUNTS_OWNER_INVALID_VALUE_GIFT_TIME = ["Duration has wrong format. Use one of these formats instead: [DD] [HH:[MM:]]ss[.uuuuuu]."]
+    PAYMENT_ACCOUNTS_OWNER_INVALID_VALUE_PAYOUT_DAY = ["A valid integer is required."]
+
+
     @staticmethod
     def key_value(key, value):
         json = {key: value}
---
+++ tests/payments/payments_accounts/owner/field_gift_time/test_payment_accounts_owner_gift_time_put.py
@@ -0,0 +1,59 @@
+import allure
+import pytest
+
+from source.api.payments.payment_accounts.owner import update_owner
+from source.base.validator import assert_status_code, assert_json_equal_json, assert_json_by_model
+from source.enums.expected import ExpectedJSON
+from source.schemas.payments.payment_accounts.owner_schema import PaymentData
+
+@allure.epic('Payments')
+@allure.feature('Payment accounts')
+@allure.story('Owner')
+@allure.suite('Test put owners')
+@pytest.mark.smoke
+
+class TestPaymentPutOwner:
+
+    @allure.title('Test payment accounts owner update valid value param gift_time')
+    @allure.description('Проверка успешного ответа [200] при изменении gift_time владельца валидными значениями')
+    @pytest.mark.parametrize("value", ["0", "11:25:45", "1", "12 11:05:33.666666"])
+    def test_payment_accounts_owner_positive_input_valid_value_param_gift_time_put(self, value):
+        """Изменение параметра  gift_time владельца валидными значениями"""
+        print("Запрос PUT")
+        payload = {
+            "commission": "08",
+            "frozen_time": "12:15:44",
+            "gift_time": f'{value}',
+            "payout_day_of_month": 4
+        }
+        response = update_owner(json=payload)
+        assert_json_by_model(response=response, model=PaymentData)
+        assert_status_code(response=response, expected=200)
+        print("Response body: " + response.text)
+
+@allure.epic('Payments')
+@allure.feature('Payment accounts')
+@allure.story('Owner')
+@allure.suite('Test put owners regression')
+@pytest.mark.regression
+
+class TestPaymentPutOwnerRegression:
+    @allure.title('Test payment accounts owner update invalid value param gift_time')
+    @allure.description('Проверка ответа [400] при изменении gift_time владельца невалидными значениями')
+    @pytest.mark.parametrize("value", ["12 11:05:33.6666555765553", "%$!@#@", "frozen","", "   ", "11:2 5:45", "  11:25:45", "11:25:45  "])
+    def test_payment_accounts_owner_positive_input_invalid_value_param_gift_time_put(self, value):
+        """Изменение параметра gift_time владельца невалидными значениями"""
+        print("Запрос PUT")
+        payload = {
+            "commission": "10",
+            "frozen_time": "12:15:44",
+            "gift_time": f'{value}',
+            "payout_day_of_month": 4
+        }
+        response = update_owner(json=payload)
+        if isinstance(value, str) and value == "":
+            pytest.xfail("failing validation actual status code 200, expected status code 400")
+        expected = ExpectedJSON.key_value(key="gift_time", value=ExpectedJSON.PAYMENT_ACCOUNTS_OWNER_INVALID_VALUE_GIFT_TIME.value)
+        assert_json_equal_json(response=response, json=expected)
+        assert_status_code(response=response, expected=400)
+        print("Response body: " + response.text)
