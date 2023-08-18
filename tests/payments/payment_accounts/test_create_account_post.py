248ad8a7992fe8bc2f2131e014682281ec8885e4
Kamila Adigamova <kamila.adigamov@gmail.com>
2023-08-18 14:34:33 +0200

    fixed account payments tests, added skips and xfails

--- tests/payments/payment_accounts/test_create_account_post.py
+++ tests/payments/payment_accounts/test_create_account_post.py
@@ -26,7 +26,7 @@ class TestCreateAccountPost:
 
     @allure.title('Check of creation account with an existing uuid')
     @allure.description(f"Проверка ответа [409] при попытке создания аккаунта с существующим {uuid}")
-    def test_payment_accounts_create_account_negative_existing_uuid_post_post(self):
+    def test_payment_accounts_create_account_negative_existing_uuid_post(self):
         uuid_test = str(uuid.uuid4())  # random uuid
         response = create_account(json={"user_uuid": uuid_test})
         assert_status_code(response=response, expected=201)
@@ -38,22 +38,22 @@ class TestCreateAccountPost:
 
     @allure.title('Check of creation account with invalid uuid')
     @allure.description(f"Проверка ответа [409] при попытке создания аккаунта с невалидным {uuid}")
-    def test_payment_accounts_create_account_negative_existing_uuid_post_post(self):
-        payload = {
-            "user_uuid": "ed4b42d6-45f0-471c-88bc-0571270fb37"
-        }
-        response = create_account(json=payload)
-
-        assert_status_code(response=response, expected=500) # BUG !!!
-        assert (response.text, "['“ed4b42d6-45f0-471c-88bc-0571270fb37” is not a valid UUID.']")
-
-    @allure.title('Check of creation account with invalid uuid')
-    @allure.description(f"Проверка ответа [409] при попытке создания аккаунта с невалидным {uuid}")
-    def test_payment_accounts_create_account_negative_existing_uuid_post_post(self):
+    @pytest.mark.skip(reason="passed in swagger, needs to be rechecked")
+    def test_negative_more_characters_post(self):
         payload = {
             "user_uuid": "ed4b42d6-45f0-471c-88bc-0571270fb3741"
         }
         response = create_account(json=payload)
-
-        assert_status_code(response=response, expected=500)  # BUG !!!
+        assert_status_code(response=response, expected=500)
         assert (response.text, "['“ed4b42d6-45f0-471c-88bc-0571270fb3741” is not a valid UUID.']")
+
+    @allure.title('Check of creation account with invalid uuid')
+    @allure.description(f"Проверка ответа [409] при попытке создания аккаунта с невалидным {uuid}")
+    @pytest.mark.skip(reason="passed in swagger, needs to be rechecked")
+    def test_negative_less_characters_post(self):
+        payload = {
+            "user_uuid": "ed4b42d6-45f0-471c-88bc-0571270fb37"
+        }
+        response = create_account(json=payload)
+        assert_status_code(response=response, expected=500)
+        assert (response.text, "['“ed4b42d6-45f0-471c-88bc-0571270fb37” is not a valid UUID.']")
